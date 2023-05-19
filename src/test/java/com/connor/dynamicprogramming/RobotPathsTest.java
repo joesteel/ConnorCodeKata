0b397d43023271be3c849c9ad7732a5788f4a115
connor culleton <connor.culleton@gmail.com>
2023-05-19 10:59:37 +0100

    adding bottom up code to robot paths problem

--- src/test/java/com/connor/dynamicprogramming/RobotPathsTest.java
+++ src/test/java/com/connor/dynamicprogramming/RobotPathsTest.java
@@ -13,12 +13,14 @@ class RobotPathsTest {
         assertEquals(1, result);
     }
 
+
     @Test
     void shouldReturn1For2x1Grid() {
         int m = 2, n = 1;
         int result = RobotPaths.uniquePaths(m,n);
         assertEquals(1, result);
     }
+
 
     @Test
     void shouldReturn2For2x2Grid() {
@@ -38,6 +40,43 @@ class RobotPathsTest {
     void shouldReturn3For2x3Grid() {
         int m = 2, n = 3;
         int result = RobotPaths.uniquePaths(m,n);
+        assertEquals(3, result);
+    }
+
+
+    @Test
+    void shouldReturn1For1x1Grid_BU() {
+        int m = 1, n =1;
+        int result = RobotPaths.uniquePathsBottomUp(m,n);
+        assertEquals(1, result);
+    }
+
+
+    @Test
+    void shouldReturn1For2x1Grid_BU() {
+        int m = 2, n = 1;
+        int result = RobotPaths.uniquePathsBottomUp(m,n);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void shouldReturn2For2x2Grid_BU() {
+        int m = 2, n =2;
+        int result = RobotPaths.uniquePathsBottomUp(m,n);
+        assertEquals(2, result);
+    }
+
+    @Test
+    void shouldReturn6For3x3Grid_BU() {
+        int m = 3, n = 3;
+        int result = RobotPaths.uniquePathsBottomUp(m,n);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void shouldReturn3For2x3Grid_BU() {
+        int m = 2, n = 3;
+        int result = RobotPaths.uniquePathsBottomUp(m,n);
         assertEquals(3, result);
     }
 
