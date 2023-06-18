9c0d29af0919c5e0f3962468da8d2e900ad7680d
connor culleton <connor.culleton@gmail.com>
2023-06-18 20:53:12 +0100

    longest increasing subsequence... working, though not they way they expected I think

---
+++ src/main/java/com/connor/dynamicprogramming/LongestSubsequence.java
@@ -0,0 +1,27 @@
+package com.connor.dynamicprogramming;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LongestSubsequence {
+
+    public static int longestSubLen(List<Integer> nums) {
+
+        return 0;
+    }
+
+    public static int longestSubLenTD(List<Integer> nums) {
+        return dfs(nums, -1, 0, 0);
+    }
+
+    public static int dfs(List<Integer> nums, int previousLevelVal, int currentLevel, int subsequenceCount) {
+        if (currentLevel == nums.size()) return subsequenceCount;
+        int left = 0;
+        if (nums.get(currentLevel) > previousLevelVal) {
+            left = dfs(nums, nums.get(currentLevel), currentLevel + 1, subsequenceCount + 1);
+        }
+        int right = dfs(nums, previousLevelVal, currentLevel + 1, subsequenceCount);
+        return Math.max(left, right);
+    }
+
+}
