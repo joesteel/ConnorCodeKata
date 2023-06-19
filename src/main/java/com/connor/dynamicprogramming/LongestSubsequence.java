56f1ec1ec7f33e4c15cc5d7cbd4bbfa817e8adc6
connor culleton <connor.culleton@gmail.com>
2023-06-19 18:52:42 +0100

    adding state to prune how many sub trees need to be executed

--- src/main/java/com/connor/dynamicprogramming/LongestSubsequence.java
+++ src/main/java/com/connor/dynamicprogramming/LongestSubsequence.java
@@ -11,16 +11,22 @@ public class LongestSubsequence {
     }
 
     public static int longestSubLenTD(List<Integer> nums) {
-        return dfs(nums, -1, 0, 0);
+        int[] state = new int[nums.size()];
+        Arrays.fill(state, Integer.MIN_VALUE);
+        return dfs(nums, -1, 0, 0, state);
     }
 
-    public static int dfs(List<Integer> nums, int lastUsedNum, int currentLevel, int subsequenceCount) {
+    public static int dfs(List<Integer> nums, int lastUsedNum, int currentLevel, int subsequenceCount, int[] state) {
         if (currentLevel == nums.size()) return subsequenceCount;
         int left = 0;
-        if (nums.get(currentLevel) > lastUsedNum) {
-            left = dfs(nums, nums.get(currentLevel), currentLevel + 1, subsequenceCount + 1);
+
+        if(state[currentLevel] <= subsequenceCount) {
+            if (nums.get(currentLevel) > lastUsedNum) {
+                left = dfs(nums, nums.get(currentLevel), currentLevel + 1, subsequenceCount + 1, state);
+                state[currentLevel] = subsequenceCount+1;
+            }
         }
-        int right = dfs(nums, lastUsedNum, currentLevel + 1, subsequenceCount);
+        int right = dfs(nums, lastUsedNum, currentLevel + 1, subsequenceCount, state);
         return Math.max(left, right);
     }
 
