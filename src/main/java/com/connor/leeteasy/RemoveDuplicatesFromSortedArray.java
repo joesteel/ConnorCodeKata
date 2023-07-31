8dd9d101d757a71f8c8d43fba9720cb1dc8a77c6
joesteel <connor.culleton@gmail.com>
2023-07-31 21:21:18 +0100

    working code

---
+++ src/main/java/com/connor/leeteasy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+package com.connor.leeteasy;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        int lPtr = 0, rPtr = 1;
+        while(rPtr < nums.length){
+            if(nums[rPtr] == nums[lPtr]) rPtr++;
+            else{
+                lPtr++;
+                nums[lPtr] = nums[rPtr];
+            }
+        }
+        return lPtr+1;
+    }
+
+}
