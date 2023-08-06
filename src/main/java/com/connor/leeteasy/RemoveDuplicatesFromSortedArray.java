8836fbe9d0a9d950ddb027d79f2d5f54d778c586
joesteel <connor.culleton@gmail.com>
2023-08-06 21:36:24 +0100

    small fix up

--- src/main/java/com/connor/leeteasy/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/connor/leeteasy/RemoveDuplicatesFromSortedArray.java
@@ -4,13 +4,13 @@ public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicates(int[] nums) {
         int lPtr = 0, rPtr = 1;
         while(rPtr < nums.length){
-            if(nums[rPtr] == nums[lPtr]) rPtr++;
-            else{
+            if(nums[rPtr] == nums[lPtr]){
+                rPtr++;
+            } else{
                 lPtr++;
                 nums[lPtr] = nums[rPtr];
             }
         }
         return lPtr+1;
     }
-
 }
