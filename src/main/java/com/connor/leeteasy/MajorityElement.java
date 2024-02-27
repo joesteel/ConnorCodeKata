89ebfcb14a1fb16b85f9d9ae157d4f50eaf5701b
joesteel <connor.culleton@gmail.com>
2024-02-27 09:02:50 +0000

    hashmap solution using memory O(n)

---
+++ src/main/java/com/connor/leeteasy/MajorityElement.java
@@ -0,0 +1,19 @@
+package com.connor.leeteasy;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = (nums.length+1)/2;
+        for (int num : nums) {
+            map.computeIfPresent(num, (key, sum) -> sum + 1);
+            map.computeIfAbsent(num, (key) -> 1);
+            if (map.get(num) >= max) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
