d763a0bc3efee64a7a40eca7043eff2ec62a12b1
joesteel <connor.culleton@gmail.com>
2024-03-10 12:57:07 +0000

    reverse and number1 bits

--- src/main/java/com/connor/leeteasy/ReverseBits.java
+++ src/main/java/com/connor/leeteasy/ReverseBits.java
@@ -4,13 +4,11 @@ public class ReverseBits {
 
     public static int reverseBits(int n) {
         int result = 0;
-        for(int i = 31; i>=0; i--){
-            int bitmask = 1 << i;
-            int isolatedBit = bitmask & n;
-            if(i>=16) isolatedBit = isolatedBit >>> (i+i-31);
-            else if(i<15) isolatedBit = isolatedBit << (31-i-i);
-            else isolatedBit = isolatedBit << 1;
-            result = isolatedBit | result;
+        for(int i = 0; i<32; i++){
+            result <<=1;
+            int isolated_bit = n & 1;
+            result = result | isolated_bit;
+            n >>= 1;
         }
         return result;
     }
