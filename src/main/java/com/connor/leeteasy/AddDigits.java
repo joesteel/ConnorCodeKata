04ce754f8ad4dbb12a3419de5acbca7cc0aef917
joesteel <connor.culleton@gmail.com>
2024-04-30 10:37:17 +0100

    brute force

---
+++ src/main/java/com/connor/leeteasy/AddDigits.java
@@ -0,0 +1,15 @@
+package com.connor.leeteasy;
+
+public class AddDigits {
+    public static int addDigits(int num) {
+        while(num >= 10){
+            int sum = 0;
+            while(num > 0){
+                sum += num % 10;
+                num = num/10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
