0b34c302e3297cc7186e6734bd028d26fbfca3ff
joesteel <connor.culleton@gmail.com>
2024-04-30 10:04:26 +0100

    replaced stack based string operations with int array and array concersion at leaf

--- src/main/java/com/connor/leeteasy/BTreePaths.java
+++ src/main/java/com/connor/leeteasy/BTreePaths.java
@@ -7,19 +7,29 @@ public class BTreePaths {
 
     public static List<String> binaryTreePaths(TreeNode root) {
         List<String> paths = new ArrayList<String>();
-        dfs(root, paths, "");
+        dfs(root, paths, new ArrayList<>());
         return paths;
     }
 
-    private static void dfs(TreeNode node, List<String> paths, String path){
+    private static void dfs(TreeNode node, List<String> paths, List<Integer> path){
         if(node == null) return;
 
-        path = path + node.val;
+        path.add(node.val);
         if(node.left == null && node.right == null) {
-            paths.add(path);
+            paths.add(pathToString(path));
         }
-        path = path + "->";
+
         dfs(node.left, paths, path);
         dfs(node.right, paths, path);
+        path.removeLast();
+    }
+
+    private static String pathToString(List<Integer> path){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < path.size(); i++){
+            sb.append(path.get(i));
+            if(i < path.size() -1) sb.append("->");
+        }
+        return sb.toString();
     }
 }
