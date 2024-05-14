91f9f62957b5610976c7620e62ea5a4eb4d09a84
joesteel <connor.culleton@gmail.com>
2024-05-14 16:47:48 +0100

    move zeros working solution

--- src/main/java/com/connor/leeteasy/FirstBadVersion.java
+++ src/main/java/com/connor/leeteasy/FirstBadVersion.java
@@ -1,2 +1,23 @@
-package com.connor.leeteasy;public class FirstBadVersion {
+package com.connor.leeteasy;
+
+public class FirstBadVersion {
+    int badVersion;
+    public FirstBadVersion(int x){
+        badVersion = x;
+    }
+    public int firstBadVersion(int n) {
+        int start = 1, end = n, firstBadVersionGuess = 0;
+        while(end - start > 1){
+            firstBadVersionGuess = ((end - start)/2) + start;
+            if(isBadVersion(firstBadVersionGuess)){
+                end = firstBadVersionGuess;
+            } else {
+                start = firstBadVersionGuess;
+            }
+        }
+        return isBadVersion(start) ? start : end;
+    }
+    private boolean isBadVersion(int version){
+        return version >= badVersion;
+    }
 }
