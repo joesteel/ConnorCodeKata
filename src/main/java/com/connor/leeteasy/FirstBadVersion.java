2c699133ba9e70b1730198207a180848096e2e55
joesteel <connor.culleton@gmail.com>
2024-05-14 08:00:17 +0100

    first bad version

--- src/main/java/com/connor/leeteasy/FirstBadVersion.java
+++ src/main/java/com/connor/leeteasy/FirstBadVersion.java
@@ -1,2 +1,27 @@
-package com.connor.leeteasy;public class FirstBadVersion {
+package com.connor.leeteasy;
+
+public class FirstBadVersion {
+
+    int badVersion;
+
+    public FirstBadVersion(int bad){
+        badVersion = bad;
+    }
+
+    public int firstBadVersion(int n) {
+        int start = 1, end = n, firstBadVersionGuess = 0;
+        while(end - start > 1){
+            firstBadVersionGuess = ((end - start)/2) + start;
+            if(isBadVersion(firstBadVersionGuess)){
+                end = firstBadVersionGuess;
+            } else {
+                start = firstBadVersionGuess;
+            }
+        }
+        return isBadVersion(start) ? start : end;
+    }
+
+    private boolean isBadVersion(int n){
+        return n >= badVersion;
+    }
 }
--- src/test/java/com/connor/leeteasy/FirstBadVersionTest.java
+++ src/test/java/com/connor/leeteasy/FirstBadVersionTest.java
@@ -1,4 +1,40 @@
+package com.connor.leeteasy;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FirstBadVersionTest {
-  
+
+    @Test
+    void shouldFindFirstBadVersion_last() {
+        int badVersion = 3;
+        FirstBadVersion fbv = new FirstBadVersion(badVersion);
+        int result = fbv.firstBadVersion(3);
+        assertEquals(badVersion, result);
+    }
+
+    @Test
+    void shouldFindFirstBadVersion_first() {
+        int badVersion = 1;
+        FirstBadVersion fbv = new FirstBadVersion(badVersion);
+        int result = fbv.firstBadVersion(3);
+        assertEquals(badVersion, result);
+    }
+
+    @Test
+    void shouldFindFirstBadVersion_second() {
+        int badVersion = 2;
+        FirstBadVersion fbv = new FirstBadVersion(badVersion);
+        int result = fbv.firstBadVersion(5);
+        assertEquals(badVersion, result);
+    }
+
+    @Test
+    void shouldFindFirstBadVersion_middle() {
+        int badVersion = 5;
+        FirstBadVersion fbv = new FirstBadVersion(badVersion);
+        int result = fbv.firstBadVersion(10);
+        assertEquals(badVersion, result);
+    }
 }
