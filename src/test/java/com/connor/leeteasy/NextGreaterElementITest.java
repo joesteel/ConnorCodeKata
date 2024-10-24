b0ebb87dc57db2d18997a687f43829fd49ea6e09
joesteel <connor.culleton@gmail.com>
2024-10-24 11:27:19 +0100

    adding greater element

--- src/test/java/com/connor/leeteasy/NextGreaterElementITest.java
+++ src/test/java/com/connor/leeteasy/NextGreaterElementITest.java
@@ -1,4 +1,24 @@
+package com.connor.leeteasy;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class NextGreaterElementITest {
-  
+
+    @Test
+    void shouldDetectNextGreatestNumbers() {
+        int[] result = NextGreaterElementI.nextGreaterElement(new int[]{2,1}, new int[]{1,3,2});
+        assertArrayEquals(new int[]{-1, 3}, result);
+        assertArrayEquals(new int[]{-1, 3}, NextGreaterElementI.nextGreaterElementUsingMonotonicStack(new int[]{2,1}, new int[]{1,3,2}));
+    }
+
+    @Test
+    void shouldDetectNextGreatestNumbers2() {
+        int[] result = NextGreaterElementI.nextGreaterElement(new int[]{4,2,1}, new int[]{1,5,2,0,4});
+        assertArrayEquals(new int[]{-1,4,5}, result);
+        assertArrayEquals(new int[]{-1,4,5}, NextGreaterElementI.nextGreaterElementUsingMonotonicStack(new int[]{4,2,1}, new int[]{1,5,2,0,4}));
+
+    }
+
 }
