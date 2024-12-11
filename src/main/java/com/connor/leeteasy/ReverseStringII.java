18bbd2850efff5eed19d26ea36c1084f53db22cd
joesteel <connor.culleton@gmail.com>
2024-12-11 14:38:48 +0000

    works O(n) but is a lot slower than the others

--- src/main/java/com/connor/leeteasy/ReverseStringII.java
+++ src/main/java/com/connor/leeteasy/ReverseStringII.java
@@ -2,15 +2,18 @@ package com.connor.leeteasy;
 
 public class ReverseStringII {
 
-    public String reverseStr(String s, int k) {
+    public static String reverseStr(String s, int k) {
         char[] chars = s.toCharArray();
-        for(int i = 0; i < chars.length; i+=k){
-            int j = (i/k)+k-1;
-            while(i % (k*2) < k){ // iterate through the open window
+        for(int i = 0; i < chars.length; i++){
+            int j = i + (k-1);
+            j = j >= chars.length ? chars.length-1 : j;
+            while( (i % (k*2) < k)){ // iterate through the open window
                 // reverse string, swap position i with i + (k-1), then i+1 with i+1 (k-1)-(i+2), then i+2 with
-                char t = chars[i];
-                chars[i] = chars[j--];
-                chars[j] = t;
+                if(i < j) {
+                    char t = chars[i];
+                    chars[i] = chars[j];
+                    chars[j] = t;
+                }
                 i++;
                 j--;
             }
