21ab41452305712b9dda6296b6cb8ac19c5a9098
joesteel <connor.culleton@gmail.com>
2025-01-21 09:14:49 +0000

    reverse words in string

--- src/test/java/com/connor/leeteasy/StudentAttendanceRecordTest.java
+++ src/test/java/com/connor/leeteasy/StudentAttendanceRecordTest.java
@@ -1,4 +1,19 @@
+package com.connor.leeteasy;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-class StudentAttendenceRecordTest {
-  
+
+class StudentAttendanceRecordTest {
+
+    @Test
+    void shouldDetermineEligible() {
+        assertFalse(StudentAttendanceRecord.checkRecord("AA"));
+        assertFalse(StudentAttendanceRecord.checkRecord("PPAPLLL"));
+        assertTrue(StudentAttendanceRecord.checkRecord("PPALLPPLPLLPP"));
+        assertFalse(StudentAttendanceRecord.checkRecord("PPALLAP"));
+        assertFalse(StudentAttendanceRecord.checkRecord("PPALLLP"));
+        assertTrue(StudentAttendanceRecord.checkRecord("PPALLP"));
+        assertTrue(StudentAttendanceRecord.checkRecord("A"));
+        assertTrue(StudentAttendanceRecord.checkRecord("LL"));
+    }
 }
