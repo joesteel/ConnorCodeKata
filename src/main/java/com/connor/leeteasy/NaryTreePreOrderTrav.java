7f1a77eb574988db9e81e2d0210882fd6838ab9f
joesteel <connor.culleton@gmail.com>
2025-04-02 20:35:28 +0100

    N-ary tree pre-order traversal

---
+++ src/main/java/com/connor/leeteasy/NaryTreePreOrderTrav.java
@@ -0,0 +1,37 @@
+package com.connor.leeteasy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaryTreePreOrderTrav {
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        dfsPreOrder(root, result);
+        return result;
+    }
+
+    public void dfsPreOrder(Node node, List<Integer> result){
+        if(node == null) return;
+
+        result.add(node.val);
+        for(Node child: node.children){
+            dfsPreOrder(child, result);
+        }
+    }
+}
