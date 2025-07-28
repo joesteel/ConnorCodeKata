0c2e54990c6ae43b17be23a089bb6485c010e96d
joesteel <connor.culleton@gmail.com>
2025-07-28 15:23:03 +0100

    small clean up for readability

--- src/main/java/com/connor/leetmedium/SearchInRotatedSortedArray.java
+++ src/main/java/com/connor/leetmedium/SearchInRotatedSortedArray.java
@@ -3,26 +3,25 @@ package com.connor.leetmedium;
 public class SearchInRotatedSortedArray {
 
     // another easy way to do this is to find the pivot and binary search in 2 parts
-    
+
     public static int search(int[] nums, int target) {
         int rotation_point=0;
         for(int i = 1; i<nums.length;i++)
             if(nums[i]<nums[i-1]) rotation_point = i;
 
         int start = 0, end = nums.length-1;
-
         while(start <= end){
             int middle = start + ((end - start)/2);
-            int rotated_index = rotatedIndex(middle, rotation_point, nums.length);
-            if(nums[rotated_index] == target) return rotated_index;
-            else if(target > nums[rotated_index]) start = middle+1;
+            int un_rotated_index = unRotateIndex(middle, rotation_point, nums.length);
+            if(nums[un_rotated_index] == target) return un_rotated_index;
+            else if(target > nums[un_rotated_index]) start = middle+1;
             else end = middle-1;
         }
 
         return -1;
     }
 
-    private static int rotatedIndex(int x, int rotation_point, int size){
+    private static int unRotateIndex(int x, int rotation_point, int size){
         int index = x+rotation_point+size;
         return index%size;
     }
--- src/test/java/com/connor/leetmedium/SearchInRotatedSortedArrayTest.java
+++ src/test/java/com/connor/leetmedium/SearchInRotatedSortedArrayTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearchInRotatedSortedArrayTest {
-
-
     @Test
     void shouldFindTarget_WhenRightSubTree_BorderCase() {
         assertEquals(2, SearchInRotatedSortedArray.search(new int[]{4,5,1,2,3}, 1));
