d3da8562c57297e376ec0e0992046592eeac438b
joesteel <connor.culleton@gmail.com>
2025-08-05 19:58:59 +0100

    sick code

---
+++ src/test/java/com/connor/leetmedium/twosumTest.java
@@ -0,0 +1,15 @@
+package com.connor.leetmedium;
+
+import com.connor.leeteasy.TwoSum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    void shouldFindTwoSum() {
+        assertArrayEquals(new int[]{1,2}, TwoSum.twoSum(new int[]{3,2,4}, 6));
+        assertArrayEquals(new int[]{0,1}, TwoSum.twoSum(new int[]{2,4,11,3}, 6));
+    }
+}
